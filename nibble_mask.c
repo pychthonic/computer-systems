523b47f69cf59ebc36a161f65d411bc3d1ef2336
pychthonic <fridgewolf@protonmail.com>
2019-11-25 14:24:44 -0500

    edited var names

--- nibble_mask.c
+++ nibble_mask.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
-int gimme_a_mask(int byte_mask_count) {
+int gimme_a_nibble_mask(int nibble_mask_count) {
     int i;
     int mask = 0;
-    for (i=0; i<byte_mask_count; i++) {
+    for (i=0; i<nibble_mask_count; i++) {
         int next_nibble = (mask + 1) * 0xf;
         mask = mask | next_nibble;
     }
@@ -14,13 +14,13 @@ int gimme_a_mask(int byte_mask_count) {
 
 
 int main () {
-    int byte_mask_count;
+    int nibble_mask_count;
     int a, mask;
     printf("Enter an int: ");
     scanf("%d", &a);
     printf("That number in hex is: %x.\nHow many of its least significant nibbles do you want to mask? ", a);
-    scanf("%d", &byte_mask_count);
-    mask = gimme_a_mask(byte_mask_count);
+    scanf("%d", &nibble_mask_count);
+    mask = gimme_a_nibble_mask(nibble_mask_count);
     a = a & mask;
     printf("Here's your masked number: 0x%x\n", a); 
     return 0;
