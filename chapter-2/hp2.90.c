8c6cb331b430c402a123f302d88b06428fcd0f55
pychthonic <fridgewolf@protonmail.com>
2021-03-24 08:05:12 -0400

    finished up hp2.90 this morning before work

--- chapter-2/hp2.90.c
+++ chapter-2/hp2.90.c
@@ -6,12 +6,6 @@
 #include "chapter2.h"
 
 
-/* 
-
-
-
-*/
-
 
 /* 
 Instead of assuming u2f, I wrote a function for it.
@@ -22,19 +16,18 @@ float u2f(unsigned u) {
     unsigned char* uptr = (unsigned char*) &u;
     unsigned char* fptr = (unsigned char*) &return_val;
 
-
     for (int i = 0; i < sizeof(float); ++i) {
         *(fptr + i) = *uptr;
         uptr++;
     }
-    printf("return_val bits at end of u2f:\n");
-    show_bits((byte_pointer) &return_val, sizeof(return_val));
-
 
     return return_val;
 }
 
-
+/* Takes an int x and returns float 2^x
+After finally getting it right, it looks simple, but this took several
+hours to figure out and get the bounds exactly right. I verified the
+numbers with an online high precision calculator. */
 float fpwr2(int x) {
     /* Result exponent and fraction */
 
@@ -43,37 +36,26 @@ float fpwr2(int x) {
 
     int shift_count;
 
-    if (x < -23) {
+    if (x < -150) {
         /* Too small. Return 0.0 */
         exp = 0;
         frac = 0;
-    } else if (x < 0) {
+    } else if (x < -127) {
         /* Denormalized result. */
-        exp = ((1 << 7) - 1) + x;
-        //frac = ((1 << 22) >> (unsigned) (-x));
-        frac = 0;
-        // shift_count = -x;
-        // frac = frac >> shift_count;
-
-
-        printf("\nfrac bits for x = %d:\n", x);
-        show_bits((byte_pointer) &frac, sizeof(frac));
-
-    } else if (x < 127) {
+        exp = 0;
+        frac = ((1 << 22) >> (-(x + 127)));
+    } else if (x < 128) {
         /* Normalized result. */
         exp = x + 127 ;
         frac = 0;
     } else {
         /* Too big. Return +inf. */
-        exp = 127;
+        exp = 255;
         frac = 0;
     }
 
     /* Pack exp and frac into 32 bits */
     u = ((exp << 23) | frac);
-    
-    printf("u bits before u2f call:\n");
-    show_bits((byte_pointer) &u, sizeof(u));
 
     /* Return as float */
     return u2f(u);
@@ -86,21 +68,15 @@ int main() {
 
     float f;
 
-    for (int x = -25; x < 5; ++x) {
+    for (int x = -152; x < 130; ++x) {
         printf("\n########################\n\n");
         f = fpwr2(x);
         printf("\nf bits in main of fptwr2(%d):\n", x);
         show_bits((byte_pointer) &f, sizeof(f));
-        printf("\nfpwr2(%d) = %f\n", x, f);
-        printf("\n###############\n");
+        printf("\nfpwr2(%d) = %.50f\n", x, f);
+        // printf("\n###############\n");
     }
-
-    printf("\n");
     
-    f = 0.125;
-    show_bits((byte_pointer) &f, sizeof(f));
-    printf("\nf = %f\n", f);
-
     printf("\n");
 
     return 0;
