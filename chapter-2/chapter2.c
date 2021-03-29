e6b81b04453ed5e919d13e55a33920fcdf7b425e
pychthonic <fridgewolf@protonmail.com>
2021-03-29 08:49:42 -0400

    getting closer, pushing for laptop

--- chapter-2/chapter2.c
+++ chapter-2/chapter2.c
@@ -61,3 +61,22 @@ void show_bits(byte_pointer start, size_t len) {
     }
     printf("\n");
 }
+
+void show_var_bits(byte_pointer start, size_t len) {
+    printf("Reordered bits:  ");
+    unsigned int bit;
+    unsigned char temp_byte;
+    for (int i=len; i>0; --i) { 
+        for (int j=8; j>0; j--) {
+            if (j == 4) {
+                printf(" ");
+            }
+            temp_byte = start[i-1] >> j - 1;
+            bit = temp_byte & 1; 
+            printf("%d", bit);
+        }
+        if ( i > 1 )
+            printf(" | ");
+    }
+    printf("\n");
+}
--- chapter-2/hp2.97.c
+++ chapter-2/hp2.97.c
@@ -35,7 +35,7 @@ to even for some numbers.
 Here are the steps we will use:
 
 1/ Push the integer into what will become the frac portion of the float bits,
-rounding to even..
+rounding to even.
 
 2/ Calculate what the exp bits should be.
 
@@ -46,6 +46,8 @@ float_bits float_i2f(int i) {
 
     unsigned sign = 0;
     unsigned u_temp = (unsigned) i;
+
+    unsigned lost_bits = 0;
 
     if (i == 0) {
         return 0;
@@ -59,48 +61,93 @@ float_bits float_i2f(int i) {
     unsigned right_shift_count = 0;
 
     unsigned M = u_temp;
-//    printf("M: %u\n", M);
+
     unsigned frac;
-    int lost_bit = 0;
+    int lost_bit = 0, last_lost_bit = 0, last_lost_bit_placed = 0;
+
+    printf("\nM bits:\n");
+    show_var_bits((byte_pointer) &M, sizeof(M));
+
+    // Shift M to the right, if necessary, so it fits into 24 bits:
     while ((M & 0xFF000000) != 0) {
-        right_shift_count += 1;
+
         if (M & 1 == 1) {
+            lost_bits = (lost_bits | (1 << right_shift_count));
             lost_bit = 1;
+            last_lost_bit = 1;
+        } else {
+            last_lost_bit = 0;
         }
-        
+
+        right_shift_count += 1;
         M >>= 1;
- //       printf("line 71");
     }
-  //  printf("right_shift_count: %u\n", right_shift_count);
-    if ((M & 1) == 1) {
-        if ((lost_bit == 1) && (right_shift_count > 0)) {
-            M += 1;
+
+    printf("\nM bits:\n");
+    show_var_bits((byte_pointer) &M, sizeof(M));
+
+    if (M == ((1 << 24) - 1)) {
+        if ((last_lost_bit) == 1) {
+            M = 0;
+            right_shift_count -= 1;
         }
     }
-   
-   // printf("M: %u\n", M);
-   // printf("i: %d\n", i);
-   // printf("\nM bits:\n");
-   // show_bits((byte_pointer) &M, sizeof(M));
+
+    printf("\nright_shift_count: %u\n", right_shift_count);
+    printf("line 84 lost_bits:\n");
+    show_var_bits((byte_pointer) &lost_bits, sizeof(lost_bits));
+
+    if (last_lost_bit == 1) {
+        last_lost_bit_placed = 1 << (right_shift_count - 1);
+        lost_bits = (lost_bits & (~last_lost_bit_placed));
+        printf("line 90 lost_bits:\n");
+        show_var_bits((byte_pointer) &lost_bits, sizeof(lost_bits));
+        printf("\nM bits:\n");
+        show_var_bits((byte_pointer) &M, sizeof(M));
+        if (lost_bits > 0) {
+            M += 1;
+        } else {
+            if (M & 1) {
+                M += 1;
+            }
+        }
+    }
+    printf("\nM bits:\n");
+    show_var_bits((byte_pointer) &M, sizeof(M));
+
+    printf("\nright_shift_count: %u\n", right_shift_count);
+    printf("\nM: %u\n", M);
+    printf("i: %d\n", i);
+    printf("\nM bits:\n");
+
+    show_var_bits((byte_pointer) &M, sizeof(M));
     
     unsigned left_shift_count = 0;
 
     while ((M & 0x00800000) == 0) {
         left_shift_count += 1;
         M <<= 1;
-        //printf("line 90");
+        // printf("line 90");
     }
-/*
-    printf("left_shift_count: %u\n", left_shift_count);
-    printf("\nM bits:\n");
-    show_bits((byte_pointer) &M, sizeof(M));
- */   
+
+    // printf("\nleft_shift_count: %u\n", left_shift_count);
+    // printf("\nM bits:\n");
+    // show_var_bits((byte_pointer) &M, sizeof(M));
+ 
     // Clear implied leading bit:
     frac = M & 0x007FFFFF;
 
-    unsigned E = 23 - left_shift_count + right_shift_count;
+    int E = 23 - left_shift_count + right_shift_count;
+
+    //printf("E = %d\n", E);
 
     unsigned exp = E + 127;
+
+    // if ((last_lost_bit == 1) && (E > 23)) {
+
+    //     printf("\nline 122\n");
+    //     frac += 1;
+    // }
 
     return ((sign << 31) | (exp << 23) | frac);
 }
@@ -118,10 +165,17 @@ int main() {
 
     float floated_int, floated_int_computer;
 
+    
 
 
+    for (i = 33554431; i <= INT_MAX; ++i) {
+        // if (i % 1000 == 0) {
+            printf("line 175, i = %d\n", i);
+        // }
+        // printf("i = %d\n", i);
+        printf("\ni bits:\n");
+        show_var_bits((byte_pointer) &i, sizeof(i));
 
-    for (i = 0; i <= 10; ++i) {
         
         fb = float_i2f(i);
         floated_int = u2f(fb);
@@ -133,13 +187,13 @@ int main() {
             printf("\n\nWhoops:\n\ni: %d\n", i);
             
             printf("\ni bits:\n");
-            show_bits((byte_pointer) &i, sizeof(i));
+            show_var_bits((byte_pointer) &i, sizeof(i));
 
             printf("\nfloated_int bits:\n");
-            show_bits((byte_pointer) &floated_int, sizeof(floated_int));
+            show_var_bits((byte_pointer) &floated_int, sizeof(floated_int));
 
             printf("\nfloated_int_computer bits:\n");
-            show_bits((byte_pointer) &floated_int_computer, sizeof(floated_int_computer));
+            show_var_bits((byte_pointer) &floated_int_computer, sizeof(floated_int_computer));
             
             printf("\n");
             
