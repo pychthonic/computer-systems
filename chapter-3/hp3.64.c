2eb8d3213eee7d11c603d649916a044686a0c456
pychthonic <fridgewolf@protonmail.com>
2021-06-09 08:00:35 -0400

    finished hp3.64.c

--- chapter-3/hp3.64.c
+++ chapter-3/hp3.64.c
@@ -16,18 +16,18 @@ long store_ele(long i, long j, long k, long *dest) {
 /*
  * In compiling the program, GCC generates this assembly code:
  *
- * store_ele:
- * leaq (%rsi,%rsi,2), %rax
- * leaq (%rsi,%rax,4), %rax     ; rax = (3 * j)*4 + j = 13 * j
- * movq %rdi, %rsi
- * salq $6, %rsi	        ; j = i * 64
- * addq %rsi, %rdi		; i += (i * 64)
- * addq %rax, %rdi		; i += rax
- * addq %rdi, %rdx		; k += i
- * movq A(,%rdx,8), %rax	; 
- * movq %rax, (%rcx)
- * movl $3640, %eax
- * ret
+ * 1  store_ele:
+ * 2  leaq (%rsi,%rsi,2), %rax
+ * 3  leaq (%rsi,%rax,4), %rax     ; rax = (3 * j)*4 + j = 13 * j
+ * 4  movq %rdi, %rsi
+ * 5  salq $6, %rsi	        ; j = i * 64
+ * 6  addq %rsi, %rdi		; i += (i * 64)
+ * 7  addq %rax, %rdi		; i += rax
+ * 8  addq %rdi, %rdx		; k += i
+ * 9  movq A(,%rdx,8), %rax	; rax = (8*k)    (L = 8 since sizeof(long) = 8)
+ * 10 movq %rax, (%rcx)            ; *dest = rax
+ * 11 movl $3640, %eax		; sizeof(entire array) = 3640
+ * 12 ret				; return 3640
  *
  * A. Extend Equation 3.1 from two dimensions to three to provide a formula for
  * the location of array element A[i][j][k].
@@ -38,7 +38,7 @@ long store_ele(long i, long j, long k, long *dest) {
  *
  * Equation 3.1 applies to any element in D with regards to that element's
  * address:
- *
+ * 
  * &D[i][j] = &D + L(C*i + j), where L is the size of data type T in bytes.
  *
  * I found it useful to use E[A][R][C] instead of A[R][S][T], so I could keep
@@ -56,11 +56,19 @@ long store_ele(long i, long j, long k, long *dest) {
  *
  * B. Use reverse engineering to determine the values of R, S, and T based on the
  * assembly.
+ * 
+ * Rewriting our formula using the relevant variable names from the book, we have:
  *
+ * &A[i][j][k] = &A + L((S*T)*i + T*j + k), where L is sizeof(element_t).
+ * 
  * For array A[R][S][T],
  * 
- * 
+ * From lines 2-3, j is multiplied by 13, so T = 13
  *
+ * From lines 4-6, i is multiplied by 65. So S*T = 65, and since T = 13, S = 5
+ *
+ * Since the total size of the 3-D array is 3640, 3640 = 8(5 * 13 * R), so
+ * R = 7
  *
  */
 
