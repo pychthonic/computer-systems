e1faffca6b63bf9e8064af1c959903f98d70cdf4
pychthonic <fridgewolf@protonmail.com>
2021-06-14 12:54:25 -0400

    finished hp3.70

--- chapter-3/hp3.70.c
+++ chapter-3/hp3.70.c
@@ -24,25 +24,25 @@ void proc (union ele *up) {
 	up->_____ = *(_____) - __________;
 }
 
-/* What are the offsets (in bytes) of the following fields:
+/* A. What are the offsets (in bytes) of the following fields:
  *
- * e1.p:
- * e1.y:
- * e2.x:
- * e2.next:
+ * e1.p: 0
+ * e1.y: 8
+ * e2.x: 0
+ * e2.next: 8
  *
  * B. How many total byes does the structure require?
  *
- *
+ * 16 bytes
  *
  * C. The compiler generates the following assembly for proc:
  *
  * proc:
- *   movq *(%rdi), %rax
- *   movq (%rax), %rdx
- *   movq (%rdx), %rdx
- *   subq 8(%rax), %rdx
- *   movq %rdx, (%rdi)
+ *   movq 8(%rdi), %rax		; rax = up.next  (address to the first byte of next node)
+ *   movq (%rax), %rdx		; rdx = *rax (so rdx = up->next.p)
+ *   movq (%rdx), %rdx		; rdx = *up->next.p
+ *   subq 8(%rax), %rdx		; rdx -= *up->next.y
+ *   movq %rdx, (%rdi)		; up->x = rdx
  *   ret
  *
  * Using this, fill in the missing expressions in the code for proc. Hint: some
@@ -51,6 +51,13 @@ void proc (union ele *up) {
  * does not perform any casting and does not violate any type constraints.
  *
  */
+
+
+void proc (union ele *up) {
+	up->x = *(up->next.p) - *(up->next.y)
+}
+
+
 
 int main() {
     	return 0;
