cbe1a4b7212bb4da5ee9697c8379e662e13ac639
pychthonic <fridgewolf@protonmail.com>
2021-06-17 21:08:10 -0400

    finished hp3.73

--- chapter-3/hp3.73.c
+++ chapter-3/hp3.73.c
@@ -1,21 +1,68 @@
 /*
  *
  * Write a function in assembly code that matches the behavior of the function find_range in
- * Figure 3.51. Your code should contain only one floating-point comparison instructoin, and
+ * Figure 3.51. Your code should contain only one floating-point comparison instruction, and
  * then it should use conditional branches to generate the correct result. Test your code on
- * all 2^32 possible argument values. Web Aside AsM:EASM on page 178 describes how to
+ * all 2^32 possible argument values. Web Aside ASM:EASM on page 178 describes how to
  * incorporate functions written in assembly code into C programs.
  *
  */
 
-
+#include <stdio.h>
+#include <assert.h>
+#include <limits.h>
+#include "../chapter-2/chapter2.h"
 
 typedef enum {NEG, ZERO, POS, OTHER} range_t;
 
-extern range_t find_range(float x);
+extern range_t find_range_book(float x);
+extern range_t my_find_range(float x);
 
 
 
 int main() {
-    	return 0;
+	unsigned u = 0, count;
+	float f;
+	range_t r_book, my_r;
+	
+        f = -5.0;
+
+	r_book = find_range_book(f);
+	my_r = my_find_range(f);
+	printf("f: %f, r_book: %d, my_r: %d\n", f, r_book, my_r);
+	
+	f = 5.0;
+
+	r_book = find_range_book(f);
+	my_r = my_find_range(f);
+	printf("f: %f, r_book: %d, my_r: %d\n", f, r_book, my_r);
+	
+	f = 0;
+
+	r_book = find_range_book(f);
+	my_r = my_find_range(f);
+	printf("f: %f, r_book: %d, my_r: %d\n", f, r_book, my_r);
+	
+	f = 0.0 / 0.0;
+
+	r_book = find_range_book(f);
+	my_r = my_find_range(f);
+	printf("f: %f, r_book: %d, my_r: %d\n", f, r_book, my_r);
+	
+
+	for (count=0; count != UINT_MAX; ++count) {
+	        f = u2f(count);	
+		r_book = find_range_book(f);
+		my_r = my_find_range(f);
+		assert (r_book == my_r);
+		if (my_r != r_book)
+			printf("r_book: %d, my_r: %d\n", r_book, my_r);
+	}
+	count = UINT_MAX;
+	f = u2f(count);	
+	r_book = find_range_book(f);
+	my_r = my_find_range(f);
+	assert (r_book == my_r);
+    	
+	return 0;
 }
--- chapter-3/hp3.73.s
+++ chapter-3/hp3.73.s
@@ -1,20 +1,41 @@
 .section .text
-.global find_range
-decode2_asm:
-  vxorps %xmm1, %xmm1, %xmm1
-  vucomiss %xmm0, %xmm1
-  ja .L5
-  vucomiss %xmm1, %xmm0
-  jp .L8
-  movl $1, %eax
-  je .L3
+.global find_range_book
+.global my_find_range
+find_range_book:
+  vxorps %xmm1, %xmm1, %xmm1  	# xmm1 = 0
+  vucomiss %xmm0, %xmm1       	# compare x to 0
+  ja .L5			# if x < 0: goto .L5	
+  vucomiss %xmm1, %xmm0		# compare 0 to x
+  jp .L8			# if x == NaN, goto .L8
+  movl $1, %eax			# if x != NaN, rax = 1
+  je .L3			# if x == 0, goto .L3
 .L8:
-  vucomiss LC0(%rip), %xmm0
-  setbe %al
-  movzbl %al, %eax
-  addl $2, %eax
-  ret
+  vucomiss %xmm1, %xmm0	        # I changed LC0(%rip) to xmm1
+  setbe %al			# if x > 0, al = 1
+  movzbl %al, %eax		# eax = al
+  addl $2, %eax			# eax += 2
+  ret				# return eax
 .L5:
-  movl $0, %eax
+  movl $0, %eax			# eax = 0
 .L3:
+  rep; ret			# return eax
+
+my_find_range:
+  vxorps %xmm1, %xmm1, %xmm1
+  vucomiss %xmm0, %xmm1		# this will be the one floating point comparison
+  jp .L1			# if x == NaN, goto .L1
+  ja .L2			# if x < 0, goto .L2
+  je .L4			# if x == 0, goto .L4
+  jb .L6			# if x > 0, goto .L6
+.L1:
+  movl $3, %eax
+  rep; ret
+.L2:
+  movl $0, %eax
+  rep; ret
+.L4:
+  movl $1, %eax
+  rep; ret
+.L6:
+  movl $2, %eax
   rep; ret
